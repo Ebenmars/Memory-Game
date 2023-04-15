9843039ec05c7cb341e4ebcbe994c3ec791b097e
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-04-15 17:51:29 -0400

    Used Png instead of Other Forms

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 var cardArray = [
 {
     name:"Ronaldo",
+    img:'images/Ronaldo.jpeg'
    
-
-    
+},
+{
+    name:"Haaland",
+    img:'images/Haaland.jpeg'
+   
+},
+{
+    name:"Lional",
+    img:'images'
+   
+},
+{
+    name:"Ronaldo",
+    img:'images/Ronaldo.jpeg'
+   
+},
+{
+    name:"Ronaldo",
+    img:'images/Ronaldo.jpeg'
+   
+},
+{
+    name:"Ronaldo",
+    img:'images/Ronaldo.jpeg'
+   
 }
+
 ]
